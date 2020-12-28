ac1848feaba67f9bcbd2d2d5a7b17fc9e2c333d0
funnydman <crazydzmitry@gmail.com>
2020-12-28 19:32:10 +0300

    Update 53-Maximum-Subarray.py

--- array/53-Maximum-Subarray.py
+++ array/53-Maximum-Subarray.py
@@ -2,7 +2,20 @@
 TODO, write divide and conquer approach
 """
 
+# Brute Force
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        m = nums[0]
+        for i in range(len(nums)):
+            s = 0
+            for j in range(i, len(nums)):
+                s += nums[j]
+                m = max(m, s)
+        return m
 
+    
 # Kadane's Algorithm
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
