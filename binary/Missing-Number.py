d9484c79517bb8b782ef17ba33215929e1355a72
funnydman <crazydzmitry@gmail.com>
2020-12-31 16:09:11 +0300

    Update Missing-Number.py

--- binary/Missing-Number.py
+++ binary/Missing-Number.py
@@ -1,3 +1,12 @@
+"""
+Some ways to solve:
+Approach #1 Sorting [Accepted]
+Approach #2 HashSet [Accepted]
+Approach #3 Bit Manipulation [Accepted]
+Approach #4 Gauss' Formula [Accepted]
+
+"""
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         l  = len(nums)
